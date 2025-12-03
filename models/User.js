697624948af3764a4746597cb4920f95cdf64a1c
thrasherdustin9904 <thrasher.dustin9904@gmail.com>
2025-12-03 17:10:52 -0500

    Create User model

---
+++ models/User.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({ username:{type:String,unique:true,required:true}, passwordHash:{type:String,required:true} },{timestamps:true});
+module.exports = mongoose.model('User', UserSchema);
