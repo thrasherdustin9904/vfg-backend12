32e63e75e4d0d40c762d1af92d38c82dd1b689e8
thrasherdustin9904 <thrasher.dustin9904@gmail.com>
2025-12-03 16:51:58 -0500

    Delete Admin.js

--- Admin.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-const AdminSchema = new mongoose.Schema({ email:{type:String,unique:true,required:true}, passwordHash:{type:String,required:true}, role:{type:String,enum:['superadmin','admin'],default:'admin'} },{timestamps:true});
-module.exports = mongoose.model('Admin', AdminSchema);
