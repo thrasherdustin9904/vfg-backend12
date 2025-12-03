cba0d466c0b93fef74332e2bb570f275ebc4cdc2
thrasherdustin9904 <thrasher.dustin9904@gmail.com>
2025-12-03 16:52:47 -0500

    Delete Product.js

--- Product.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-const ProductSchema = new mongoose.Schema({ name:{type:String,required:true}, description:String, price:{type:Number,required:true}, image:String, thumb:String },{timestamps:true});
-module.exports = mongoose.model('Product', ProductSchema);
