278652c0deef3e437462642490d17a4c226a37ac
thrasherdustin9904 <thrasher.dustin9904@gmail.com>
2025-12-04 01:31:27 -0500

    Update Settings.js

--- src/models/Settings.js
+++ src/models/Settings.js
@@ -1,9 +1,5 @@
-import mongoose from "mongoose";
-
-const settingsSchema = new mongoose.Schema({
-  payment: {
-    paypalEmail: { type: String, default: "" }
-  }
-});
-
-export default mongoose.model("Settings", settingsSchema);
+export default {
+  currency: "USD",
+  paypalClientId: process.env.PAYPAL_CLIENT_ID,
+  paypalSecret: process.env.PAYPAL_SECRET
+};
