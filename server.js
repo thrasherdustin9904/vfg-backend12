fabb358edde79a39b9534d598b5007079a5d1d71
thrasherdustin9904 <thrasher.dustin9904@gmail.com>
2025-12-03 23:06:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,9 @@ app.get("/api/health", (req, res) => {
 import settingsRoutes from "./routes/settingsRoutes.js";
 app.use("/api/admin/settings", settingsRoutes);
 
+import paypalRoutes from "./routes/paypalRoutes.js";
+app.use("/api/payments/paypal", paypalRoutes);
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
